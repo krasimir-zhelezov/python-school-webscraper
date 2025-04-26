d520c497dc237d4c65bfb7522c956cd147eb9b37
krasimir-zhelezov <krasizh07@gmail.com>
2025-04-26 14:45:44 +0300

    backend(feat): search for math endpoint

--- backend/app/routers/search_router.py
+++ backend/app/routers/search_router.py
@@ -1,7 +1,12 @@
 from fastapi import APIRouter, Depends, HTTPException
 
+from app.services.webscraper import WebScraper
+
 search_router = APIRouter()
 
 @search_router.get("/search/math/{query}")
 async def search_for_math(query):
-    return f'searching for {query}'
+    webscraper = WebScraper()
+    data = webscraper.search_for_math(query)
+    
+    return data
