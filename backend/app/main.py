458978c691678318ddeb264531e03de5d6cacfbc
krasimir-zhelezov <krasizh07@gmail.com>
2025-04-26 19:03:43 +0300

    backend(fix): faster responses

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,9 +1,13 @@
 from fastapi import FastAPI
 from app.routers.search_router import search_router
+from app.services.gemini_bot import GeminiBot
+from app.services.webscraper import WebScraper
 from dotenv import load_dotenv
 
 load_dotenv()
 
 app = FastAPI(title="FastAPI with Selenium & Gemini")
+app.state.webscraper = WebScraper()
+app.state.gemini_bot = GeminiBot()
 
 app.include_router(search_router, prefix='/api')
--- backend/app/routers/search_router.py
+++ backend/app/routers/search_router.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends, HTTPException, Request
 
 from app.services.webscraper import WebScraper
 from app.services.gemini_bot import GeminiBot
@@ -6,21 +6,23 @@ from app.services.gemini_bot import GeminiBot
 search_router = APIRouter()
 
 @search_router.get("/search/math/{query}")
-async def search_for_math(query):
-    webscraper = WebScraper()
+async def search_for_math(query, request: Request):
+    webscraper = request.app.state.webscraper
+    gemini_bot = request.app.state.gemini_bot
+    
     raw_data = webscraper.search_for_math(query)
     
-    gemini_bot = GeminiBot()
     data = gemini_bot.process_data(raw_data)
     
     return data
 
 @search_router.get("/search/bg/{query}")
-async def search_for_bulgarian(query):
-    webscraper = WebScraper()
+async def search_for_bulgarian(query, request: Request):
+    webscraper = request.app.state.webscraper
+    gemini_bot = request.app.state.gemini_bot
+    
     raw_data = webscraper.search_for_bulgarian(query)
     
-    gemini_bot = GeminiBot()
     data = gemini_bot.process_data(raw_data)
     
     return data
