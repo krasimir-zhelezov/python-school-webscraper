46775b5ad19cf3076deb1db2f09a8863701cd840
krasimir-zhelezov <krasizh07@gmail.com>
2025-04-26 14:36:17 +0300

    chore: added .gitignore

--- main.py
+++
@@ -1,9 +0,0 @@
-from webscraper import WebScraper
-
-webscraper = WebScraper()
-
-#webscraper.search_for_bulgarian('шампоан')
-webscraper.search_for_math('равностранен триъгълник')
-
-while True:
-    ...
